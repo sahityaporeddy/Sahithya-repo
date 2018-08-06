51838917a793778e578661afc04ecb4a97665c78
saporedd <saporedd@DIN73003017.corp.capgemini.com>
2018-08-06 14:00:59 +0530

    Commit1

---
+++ Day1-Git11/src/main/java/org/cap/demo/MainClass.java
@@ -0,0 +1,14 @@
+package org.cap.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hellooooo world");
+		greet();
+	}
+	public static void greet() {
+		System.out.println("Hellooooo...gm");
+
+	}
+
+}
